5ffd2022d102c374c3c17245575864e168053adf
Miguel Rodriguez <ultralord239@gmail.com>
2022-01-11 08:23:21 +0900

    added icon and tab name

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
 
+
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -16,3 +17,4 @@ const app = firebase.initializeApp({
 export const auth = app.auth();
 const storage = firebase.storage;
 export { storage, app as default };
+
